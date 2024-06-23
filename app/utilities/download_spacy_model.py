fc1bc1de98ca5fe46b813f58ef521ae202d6c991
amnotme <leopoldo.hernandez.oliva@gmail.com>
2024-06-23 17:43:08 -0400

    Updating the error handling

--- app/utilities/download_spacy_model.py
+++ app/utilities/download_spacy_model.py
@@ -5,7 +5,7 @@ import spacy
 def download_and_load_spacy_model():
     try:
         nlp = spacy.load("en_core_web_sm")
-    except OSError:
+    except OSError | IOError:
         subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
         nlp = spacy.load("en_core_web_sm")
     return nlp
